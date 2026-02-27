506187058c85e272d7395b25564bb07eaf42e6f4
ryancoderjackson <rc70jackson@gmail.com>
2026-02-27 01:36:40 -0600

    Update build.sh file with seed_demo

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py seed_demo
